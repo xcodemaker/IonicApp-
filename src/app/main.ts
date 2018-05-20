49bf9b435d9d24500856ee0fa9d5b0df58448943
dhammika Saman Kumara <dhammika.dsk@gmail.com>
2018-05-20 23:11:29 +0530

    pouchdb sync not working

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
-import 'hammerjs';
+// import 'hammerjs';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
